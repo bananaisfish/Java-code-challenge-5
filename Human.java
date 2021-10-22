256a86f405c7c2dd48673ca1dc24c6431825f95d
Teguh Putra Mamonto <91592208+bananaisfish@users.noreply.github.com>
2021-10-22 17:05:50 +0700

    Add files via upload

---
+++ Human.java
@@ -0,0 +1,10 @@
+public class Human {
+    String name;
+    int age;
+  
+    public void walk() {
+      System.out.println(name + " sedang berjalan...");
+    
+    }
+  
+}
---
+++ Main.java
@@ -0,0 +1,47 @@
+class Main {
+    public static void main(String[] args) {
+      Hero hero = new Hero("Teguh", 21);
+
+      hero.player();
+
+    for(int i=1; i<=10;i++){
+        hero.walk();
+        }
+    
+    for(int i=1; i<=2;i++){
+        hero.run();
+        }
+    for(int i=1; i<=3;i++){
+        hero.eat();
+        }
+    for(int i=1; i<=30;i++){
+        hero.walk();
+        }
+    for(int i=1; i<=5;i++){
+        hero.drink();
+        }
+    for(int i=1; i<=3;i++){
+         hero.jump();
+        }
+    for(int i=1; i<=5;i++){
+        hero.sit();
+        }
+    for(int i=1; i<=6;i++){
+        hero.attack();
+        }   
+    
+    //   hero.walk();
+    //   hero.walk();
+    //   hero.walk();
+    //   hero.run();
+    //   hero.run();
+    //   hero.run();
+  
+      
+  
+     // hero.player();
+  
+   
+    }
+}              
+
